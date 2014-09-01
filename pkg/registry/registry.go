a96ca4937e969f575648281b86d34b5b97805c5c
Albert Zhang <zhgwenming@gmail.com>
2014-09-01 22:27:20 +0800

    subnet enum

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -1,10 +1,12 @@
 package registry
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/spf13/cobra"
 	"log"
+	"net"
 	"strings"
 	"time"
 )
@@ -41,6 +43,42 @@ type etcdRegistry struct {
 	etcdClient *etcd.Client
 }
 
+func GetAllSubnet(ipnet *net.IPNet, hostbits int) []net.IPNet {
+	ones, bits := ipnet.Mask.Size()
+	zeros := bits - ones
+
+	// network bits
+	netBits := zeros - hostbits
+	if netBits < 0 {
+		return []net.IPNet{}
+	}
+
+	ip4 := ipnet.IP.To4()
+
+	numberSubnet := 1 << uint(netBits)
+	subnet := make([]net.IPNet, numberSubnet)
+
+	for i := uint32(0); i < uint32(numberSubnet); i++ {
+		ipbuf := make([]byte, 4)
+		number := i << uint(hostbits)
+		binary.BigEndian.PutUint32(ipbuf, number)
+
+		ip := (((uint32(ipbuf[0]) | uint32(ip4[0])) << 24) |
+			((uint32(ipbuf[1]) | uint32(ip4[1])) << 16) |
+			((uint32(ipbuf[2]) | uint32(ip4[2])) << 8) |
+			uint32(ipbuf[3]) | uint32(ip4[3]))
+		binary.BigEndian.PutUint32(ipbuf, ip)
+
+		ipmask := net.CIDRMask(bits-hostbits, bits)
+
+		subipnet := net.IPNet{ipbuf, ipmask}
+		subnet = append(subnet, subipnet)
+	}
+
+	return subnet
+
+}
+
 func registryInit(cmd *cobra.Command, args []string) {
 
 	if len(args) > 0 {
@@ -49,7 +87,13 @@ func registryInit(cmd *cobra.Command, args []string) {
 		subnet = DEFAULT_SUBNET
 	}
 
-	fmt.Printf("vrouter init %s, etcd: %s\n", subnet, etcdServers)
+	_, ipnet, err := net.ParseCIDR(subnet)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("vrouter init %s, %v, etcd: %s\n", subnet, ipnet, etcdServers)
 	etcd := etcd.NewClient(etcdServers)
 	client = etcdRegistry{etcdClient: etcd}
 }
