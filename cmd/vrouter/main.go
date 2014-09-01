f12f39496609f7fb1751e67d98bb9965dac70faf
Albert Zhang <zhgwenming@gmail.com>
2014-09-01 11:16:33 +0800

    put init cmd into registry pkg

--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/spf13/cobra"
+	"github.com/zhgwenming/vrouter/pkg/registry"
 	"log"
 )
 
 var (
 	daemon bool
-	subnet string
 )
-
-const (
-	DEFAULT_SUBNET = "10.0.0.0/16"
-)
-
-func registryInit(cmd *cobra.Command, args []string) {
-
-	if len(args) > 0 {
-		subnet = args[0]
-	} else {
-		subnet = DEFAULT_SUBNET
-	}
-
-	fmt.Printf("vrouter init %s\n", subnet)
-}
 
 func main() {
 	routerCmd := &cobra.Command{
@@ -37,14 +22,7 @@ func main() {
 
 	routerCmd.Flags().BoolVarP(&daemon, "daemon", "d", true, "whether to run as daemon mode")
 
-	initCmd := &cobra.Command{
-		Use:   "init [subnet]",
-		Short: "init the registry",
-		Long:  "init the registry with speciffic ip network information",
-		Run:   registryInit,
-	}
-
-	routerCmd.AddCommand(initCmd)
+	registry.Init(routerCmd)
 
 	if err := routerCmd.Execute(); err != nil {
 		log.Fatal(err)
---
+++ pkg/registry/registry.go
@@ -0,0 +1,41 @@
+package registry
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	machines  string
+	hostNames []string
+	subnet    string
+)
+
+const (
+	DEFAULT_SUBNET = "10.0.0.0/16"
+)
+
+func Init(parent *cobra.Command) {
+
+	initCmd := &cobra.Command{
+		Use:   "init [subnet]",
+		Short: "init the registry",
+		Long:  "init the registry with speciffic ip network information",
+		Run:   registryInit,
+	}
+
+	initCmd.Flags().StringVarP(&machines, "machines", "m", "", "List of machines")
+
+	parent.AddCommand(initCmd)
+}
+
+func registryInit(cmd *cobra.Command, args []string) {
+
+	if len(args) > 0 {
+		subnet = args[0]
+	} else {
+		subnet = DEFAULT_SUBNET
+	}
+
+	fmt.Printf("vrouter init %s\n", subnet)
+}
