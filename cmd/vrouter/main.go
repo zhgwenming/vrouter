e5fac62fcf53b5a6d01c75575960863bdfea467f
Albert Zhang <zhgwenming@gmail.com>
2014-10-30 17:32:40 +0800

    use hostname inside tls crt

--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	//"fmt"
 	"github.com/zhgwenming/vrouter/controller"
 	"github.com/zhgwenming/vrouter/daemon"
 	"github.com/zhgwenming/vrouter/registry"
 	"log"
+	"os"
 )
 
 func main() {
@@ -19,10 +19,15 @@ func main() {
 	routerCmd := cmd.InitCmd(etcdConfig)
 	routerCmd.PersistentFlags().StringVarP(&etcdConfig.Servers, "etcd_servers", "e", "https://127.0.0.1:4001", "etcd server uri")
 
+	tlsName := "tls"
+	if hostname, err := os.Hostname(); err == nil {
+		tlsName = hostname
+	}
+
 	// cafile/certfile/keyfile
 	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CaFile, "ca-file", "a", "", "etcd server ca file")
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CertFile, "cert-file", "t", "/etc/vrouter/tls.crt", "etcd server cert file")
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.KeyFile, "key-file", "k", "/etc/vrouter/tls.key", "etcd server key file")
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CertFile, "cert-file", "t", "/etc/vrouter/"+tlsName+".crt", "etcd server cert file")
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.KeyFile, "key-file", "k", "/etc/vrouter/"+tlsName+".key", "etcd server key file")
 
 	controller.InitCmd(routerCmd, etcdConfig)
 
