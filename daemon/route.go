c482e7ed04f6c26c3521e8ef5ac69f7f59013925
Albert Zhang <zhgwenming@gmail.com>
2014-11-01 16:21:46 +0800

    use gateway in route

--- daemon/route.go
+++ daemon/route.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Route struct {
-	target string
-	gw     string
+	target  string
+	gateway string
 }
 
-func NewRoute(target, gw string) *Route {
-	return &Route{target: target, gw: gw}
+func NewRoute(target, gateway string) *Route {
+	return &Route{target: target, gateway: gateway}
 }
 
 func ParseRoute(str string) (*Route, error) {
@@ -21,13 +21,13 @@ func ParseRoute(str string) (*Route, error) {
 	if len(r) != 2 {
 		return nil, errors.New("Wrong route format")
 	}
-	return &Route{target: r[0], gw: r[1]}, nil
+	return &Route{target: r[0], gateway: r[1]}, nil
 }
 
 func (r *Route) AddRoute(iface *net.Interface) error {
-	return netlink.AddRoute(r.target, "", r.gw, iface.Name)
+	return netlink.AddRoute(r.target, "", r.gateway, iface.Name)
 }
 
 func (r *Route) String() string {
-	return r.target + ":" + r.gw
+	return r.target + ":" + r.gateway
 }
