16a9d9b1fe98a9a8ef27605949d61647b7548e2d
Albert Zhang <zhgwenming@gmail.com>
2014-11-10 15:46:44 +0800

    cli WIP

--- controller/cli.go
+++ controller/cli.go
@@ -1,5 +1,10 @@
 package controller
 
+import (
+	"github.com/zhgwenming/vrouter/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
+	"github.com/zhgwenming/vrouter/Godeps/_workspace/src/github.com/spf13/cobra"
+)
+
 type cli struct {
 	etcdClient *etcd.Client
 }
