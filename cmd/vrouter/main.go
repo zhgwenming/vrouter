44e9d55e243a4c23c28bd1eeb45faa2fbf373887
Albert Zhang <zhgwenming@gmail.com>
2014-11-11 20:48:14 +0800

    default {ca,cert,key}-file from env

--- cmd/vrouter/main.go
+++ cmd/vrouter/main.go
@@ -6,9 +6,41 @@ import (
 	"github.com/zhgwenming/vrouter/registry"
 	"log"
 	"os"
+	"syscall"
 )
 
 func main() {
+
+	tlsName := "tls"
+	if hostname, err := os.Hostname(); err == nil {
+		tlsName = hostname
+	}
+
+	// default tls cert info
+	caFile := ""
+	certFile := "/etc/vrouter/" + tlsName + ".crt"
+	keyFile := "/etc/vrouter/" + tlsName + ".key"
+	etcdServers := "https://127.0.0.1:4001"
+
+	if file, found := syscall.Getenv("CA_FILE"); found {
+		caFile = file
+	}
+
+	if file, found := syscall.Getenv("CERT_FILE"); found {
+		certFile = file
+	}
+
+	if file, found := syscall.Getenv("KEY_FILE"); found {
+		keyFile = file
+	}
+
+	if key, found := syscall.Getenv("ETCD_SERVERS"); found {
+		etcdServers = key
+	}
+
+	if certFile == "" || keyFile == "" {
+		etcdServers = "http://127.0.0.1:4001"
+	}
 
 	// etcd client
 	etcdConfig := new(registry.ClientConfig)
@@ -17,17 +49,12 @@ func main() {
 	cmd := daemon.NewConfig()
 
 	routerCmd := cmd.InitCmd(etcdConfig)
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.Servers, "etcd_servers", "e", "https://127.0.0.1:4001", "etcd server uri")
-
-	tlsName := "tls"
-	if hostname, err := os.Hostname(); err == nil {
-		tlsName = hostname
-	}
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.Servers, "etcd_servers", "e", etcdServers, "etcd server uri")
 
 	// cafile/certfile/keyfile
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CaFile, "ca-file", "", "", "etcd server ca file")
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CertFile, "cert-file", "", "/etc/vrouter/"+tlsName+".crt", "etcd server cert file")
-	routerCmd.PersistentFlags().StringVarP(&etcdConfig.KeyFile, "key-file", "", "/etc/vrouter/"+tlsName+".key", "etcd server key file")
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CaFile, "ca-file", "", caFile, "etcd server ca file")
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.CertFile, "cert-file", "", certFile, "etcd server cert file")
+	routerCmd.PersistentFlags().StringVarP(&etcdConfig.KeyFile, "key-file", "", keyFile, "etcd server key file")
 
 	controller.AddSubCommands(routerCmd, etcdConfig)
 
